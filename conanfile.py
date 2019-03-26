503513a4fcbaa45564090a221ba00395c62ab130
Martin Nordsletten <mnordsletten@gmail.com>
2019-03-26 09:09:01 +0100

    Conan: Now using python_requires for git function + major_mode

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,11 @@
-from conans import ConanFile,tools,CMake
+from conans import ConanFile, python_requires, CMake
 
-def get_version():
-    git = tools.Git()
-    try:
-        prev_tag = git.run("describe --tags --abbrev=0")
-        commits_behind = int(git.run("rev-list --count %s..HEAD" % (prev_tag)))
-        # Commented out checksum due to a potential bug when downloading from bintray
-        #checksum = git.run("rev-parse --short HEAD")
-        if prev_tag.startswith("v"):
-            prev_tag = prev_tag[1:]
-        if commits_behind > 0:
-            prev_tag_split = prev_tag.split(".")
-            prev_tag_split[-1] = str(int(prev_tag_split[-1]) + 1)
-            output = "%s-%d" % (".".join(prev_tag_split), commits_behind)
-        else:
-            output = "%s" % (prev_tag)
-        return output
-    except:
-        return '0.0.0'
+conan_tools = python_requires("conan-tools/[>=1.0.0]@includeos/stable")
 
 class MicroLBConan(ConanFile):
     settings= "os","arch","build_type","compiler"
     name = "microlb"
-    version=get_version()
+    version = conan_tools.git_get_semver()
     license = 'Apache-2.0'
     description = 'Run your application with zero overhead'
     generators = 'cmake'
@@ -35,9 +18,6 @@ class MicroLBConan(ConanFile):
         "revision" : "auto"
     }
 
-    default_user="includeos"
-    default_channel="test"
-
     options={
         "liveupdate":[True,False],
         "tls": [True,False]
@@ -46,13 +26,14 @@ class MicroLBConan(ConanFile):
         "liveupdate":True,
         "tls":True
     }
+
+    def package_id(self):
+        self.info.requires.major_mode()
+
     def requirements(self):
-        self.requires("includeos/[>=0.14.0,include_prerelease=True]@{}/{}".format(self.user,self.channel))
+        self.requires("includeos/[>=0.14.0,include_prerelease=True]@includeos/latest")
         if (self.options.liveupdate):
-            self.requires("liveupdate/[>=0.14.0,include_prerelease=True]@{}/{}".format(self.user,self.channel))
-        if (self.options.tls):
-            #this will put a dependency requirement on openssl
-            self.requires("s2n/1.1.1@{}/{}".format(self.user,self.channel))
+            self.requires("liveupdate/[>=0.14.0,include_prerelease=True]@includeos/latest")
 
     def _arch(self):
         return {
